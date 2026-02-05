ad5f39c73021a4bb76a4c8cdf15c426c46df0508
jitseV <jitse.verstraete@gmail.com>
2026-02-05 23:08:39 +0100

    first component

---
+++ components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Hero.css';
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <div className="hero-content">
+                <h1>Welcome to Jitse's Portfolio</h1>
+                <p>Game Developer</p>
+                <button>Resume</button>
+            </div>
+        </section>
+    );
+}
